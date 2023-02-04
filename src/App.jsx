2e28afa774057fb066df172704ac8484943a6ff6
honglc <hongmainmail@gmail.com>
2023-02-04 16:37:08 +0700

    init code

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import "./css/custom.css";
+import "./css/style.default.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Footer from "./Share/Footer/Footer";
+import Header from "./Share/Header/Header";
+import Home from "./Home/Home";
+import Detail from "./Detail/Detail";
+import Cart from "./Cart/Cart";
+import SignIn from "./Authentication/SignIn";
+import SignUp from "./Authentication/SignUp";
+import Checkout from "./Checkout/Checkout";
+import History from "./History/History";
+import Shop from "./Shop/Shop";
+import Chat from "./Share/Chat/Chat";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/detail/:id" element={<Detail />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/history" element={<History />} />
+          <Route exact path="/shop" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Chat /> */}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
