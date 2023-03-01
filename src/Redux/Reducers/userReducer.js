a85f1e57cf50bf3f0b69cb461cef666b037c72e5
honglc <hongmainmail@gmail.com>
2023-03-01 14:53:54 +0700

    add role attributes (decentralization) to user model

--- src/Redux/Reducers/userReducer.js
+++ src/Redux/Reducers/userReducer.js
@@ -3,6 +3,7 @@ import { USER } from "../Actions/Types/userType";
 const initialState = {
   userId: {},
   cookie: {},
+  role: "",
 };
 
 const reducer = (state = initialState, action) => {
@@ -10,10 +11,10 @@ const reducer = (state = initialState, action) => {
     case USER.SIGN_IN:
       state.userId = action.payload.userId;
       state.cookie = action.payload.cookie;
-
+      state.role = action.payload.role;
       return { ...state };
     case USER.SIGN_OUT:
-      state.userId = "";
+      state = {};
       return { ...state };
     default:
       return state;
