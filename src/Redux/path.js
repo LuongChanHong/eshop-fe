a0732e9c5af7ba685bcfdf98b96be2471560fd0f
honglc <hongmainmail@gmail.com>
2023-03-30 11:35:16 +0700

    check cookies before calling some API

--- src/Redux/path.js
+++ src/Redux/path.js
@@ -1,2 +1,2 @@
-export const serverPath = "http://localhost:5000";
-// export const serverPath = "https://eshop-be-dw04.onrender.com";
+// export const serverPath = "http://localhost:5000";
+export const serverPath = "https://eshop-be-dw04.onrender.com";
